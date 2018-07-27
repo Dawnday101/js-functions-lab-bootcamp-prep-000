938457789b6d5ab3906fa9355d3129caa141d042
dawnday101@gmail.com <dawnday101@gmail.com>
2018-07-27 05:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,17 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-var name = 'you';
 
 function happyHolidaysTo(string) {
+  var name = 'you'
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(string) {
+  var name = 'you'
+  var holiday = 'Independence Day'
+  
   return `Happy ${holiday}, ${name}!'
-}}
+}
 function holidayCountdown(string) {
   return "It's ${days} days until ${holiday}!"
 }
